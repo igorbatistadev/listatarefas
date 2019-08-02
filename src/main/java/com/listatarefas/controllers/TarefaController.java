cf507fe8ec0b4117aae6b28650f991ab1d9a1a81
Igor Santana <igoricm1@gmail.com>
2019-08-01 22:42:18 -0300

    Implementação dos métodos responsáveis por finalizar tarefa e buscar tarefas ativas e finalizadas; Alteração no index.html

--- src/main/java/com/listatarefas/controllers/TarefaController.java
+++ src/main/java/com/listatarefas/controllers/TarefaController.java
@@ -4,10 +4,13 @@ import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.listatarefas.models.Enum.Status;
 import com.listatarefas.models.Tarefa;
 import com.listatarefas.repositories.TarefaRepository;
 
@@ -25,8 +28,10 @@ public class TarefaController {
 	@RequestMapping(path="/tarefas")
 	public ModelAndView tarefas() {
 		ModelAndView mv = new ModelAndView("index");
-		Iterable<Tarefa> tarefas = tr.findAll();
-		mv.addObject("tarefas", tarefas);
+		Iterable<Tarefa> tarefasafazer = tr.buscaTarefas("AFAZER");
+		Iterable<Tarefa> tarefasconcluidas = tr.buscaTarefas("CONCLUIDA");
+		mv.addObject("tarefasafazer", tarefasafazer);
+		mv.addObject("tarefasconcluidas", tarefasconcluidas);
 		return mv;
 	}
 	
@@ -41,6 +46,16 @@ public class TarefaController {
 		tarefa.setData_criacao(Calendar.getInstance().getTime());
 		tr.save(tarefa);
 		
+		return "redirect:/tarefas";
+	}
+	
+	@RequestMapping(path="/finalizar")
+	public String finalizar(@RequestParam("id") Long id, ModelMap model) {
+		
+		Tarefa tarefa = tr.getOne(id);
+		tarefa.setStatus(Status.CONCLUIDA);
+		tarefa.setData_conclusao(Calendar.getInstance().getTime());
+		tr.saveAndFlush(tarefa);
 		return "redirect:/tarefas";
 	}
 	
--- src/main/java/com/listatarefas/repositories/TarefaRepository.java
+++ src/main/java/com/listatarefas/repositories/TarefaRepository.java
@@ -1,9 +1,13 @@
 package com.listatarefas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.listatarefas.models.Tarefa;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long>{
+
+	@Query(value = "select t.* from tarefa t where t.status = ?1", nativeQuery = true)
+	Iterable<Tarefa> buscaTarefas(String status);
 
 }
